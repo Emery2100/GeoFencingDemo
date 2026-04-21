8b0444aceb7fb93c281d0b577943fd112a791b31
David Emery <Emery2100@yahoo.com>
2026-04-20 20:51:55 -0700

    Initial Commit

---
+++ GeoFencingDemo/GeoFencingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoFencingDemoApp.swift
+//  GeoFencingDemo
+//
+//  Created by David Emery on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GeoFencingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
